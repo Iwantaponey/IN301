a450fabd6329a8adb348cf90953bb36c146277cf
user <user@user-VirtualBox.(none)>
2015-09-24 12:55:37 +0200

    j'ai modifié td1ex2.c

--- td0/td1ex2.c
+++ td0/td1ex2.c
@@ -1,80 +1,44 @@
 #include <stdio.h>
+
 int main()
 {
-
 	printf("Veuillez entrer un nombre de secondes \n");
-	int check,heure,minute,seconde,entree;
-	check = scanf("%i", &entree);
-	if ((check <1) || (entree<0)) 
+	int check,temps;
+	check = scanf("%i", &temps);
+	if ((check <1) || (temps<0)) 
 	{
 		printf("Vous n'avez pas entré un nombre entier positif ! \n Veuillez recommencer.\n");
 		
 		return 0; 
 	}
-	if ((check>0) || (entree>0))
+	if ((check>0) || (temps>0))
 	{
-		if (entree<60) 
-		{
-			heure=0;
-			minute=0;
-			seconde=entree;
-				if (seconde<2)
-					{
-						printf("%i secondes correspond à %i heure %i minute %i seconde ",entree, heure, minute, seconde);
-					}
-				else 
-						printf("%i secondes correspond à %i heure %i minute %i secondes ",entree, heure, minute, seconde);
-		}
-		if (entree>=60 && entree<3600)
-		{
-			heure=0;
-			minute= entree/60 ;
-			seconde=(entree%60);
-				if (minute<2 && seconde<2)
-					{
-						printf("%i secondes correspond à %i heure %i minute %i seconde ",entree, heure, minute, seconde);
-					}
-				if (minute>2 && seconde<2)
-					{
-							printf("%i secondes correspond à %i heure %i minutes %i seconde ",entree, heure, minute, seconde);
-					}
-				if (minute>2 && seconde>2)
-					{
-						printf("%i secondes correspond à %i heure %i minutes %i secondes ",entree, heure, minute, seconde);
-					}
-		}
-		if (entree>=3600)
-		{
-			heure=entree/3600 ;
-			minute = (entree-(entree/3600))/60 ;
-			seconde = (entree%60);
-		
-				if (heure<2 && minute<2 && seconde<2)
-					{
-						printf("%i secondes correspond à %i heure %i minute %i seconde ",entree, heure, minute, seconde);
-					}
-				if (heure<2 && minute>2 && seconde<2)
-					{
-						printf("%i secondes correspond à %i heure %i minutes %i seconde ",entree, heure, minute, seconde);
-					}
-				if (heure<2 && minute>2 && seconde>2)
-					{
-						printf("%i secondes correspond à %i heure %i minutes %i secondes ",entree, heure, minute, seconde);
-					}
-				if (heure>2 && minute<2 && seconde<2)
-					{
-						printf("%i secondes correspond à %i heures %i minute %i seconde ",entree, heure, minute, seconde);
-					}
-				if (heure>2 && minute>2 && seconde<2)
-					{
-							printf("%i secondes correspond à %i heures %i minutes %i seconde ",entree, heure, minute, seconde);
-					}
-				if (heure>2 && minute>2 && seconde>2)
-					{
-						printf("%i secondes correspond à %i heures %i minutes %i secondes ",entree, heure, minute, seconde);
-					}
-		}
-	}
 	
-	return 0;
-}	
+		int heure, min, sec;
+		heure=temps/3600;
+		min=(temps-3600*heure)/60;
+		sec=temps-3600*heure-60*min;
+		
+		if (heure>1 && min>1 && sec>1)
+		{
+			printf("%d secondes correspond à %d heures %d minutes %d secondes" ,temps, heure, min, sec);
+		}
+		
+		else if (min>1 && sec>1)
+			{
+				printf("%d secondes correspond à %d heure %d minutes %d secondes" ,temps, heure, min, sec);
+			}
+		
+		else if (sec>1)
+			{
+				printf("%d secondes correspond à %d heure %d minute %d secondes" ,temps, heure, min, sec);
+			}
+		
+		else
+			{
+				printf("%d seconde correspond à %d heure %d minute %d seconde" ,temps, heure, min, sec);
+			}
+		
+		return 0;
+	}
+}
