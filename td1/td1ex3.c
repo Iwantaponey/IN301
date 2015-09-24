f12c8ffe0aae504a223dc8c1872cd206b0a566f4
user <user@user-VirtualBox.(none)>
2015-09-24 22:12:19 +0200

    j'ai ajouté les fichiers suivants dans le fichier td1

---
+++ td1/td1ex3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main ()
+{
+	printf("Veuillez entrer deux entiers positifs \n");
+	int x,y,checkx,checky,resultat;
+	checkx = scanf("%i",&x);
+	checky = scanf("%i",&y);
+	if (checkx<1 || checky<1)
+		{
+			printf("Vous n'avez pas entré un entier positif ! Veuillez recommencer\n");
+			return 0;
+		}
+	else
+	{
+		if (x%2==0)
+		{
+			
+		}
+		
+	}
+	
+	return 0;
+}
