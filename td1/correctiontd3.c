0f84050874cb05756efe1fa54a0527d4ab6fa341
Julie <juju0397@msn.com>
2015-10-01 12:48:56 +0200

    J'ai ajouté la correction en algorithmique de l exercice 3 du td1

---
+++ td1/correctiontd3.c
@@ -0,0 +1,12 @@
+// en algorithmique
+//	multieg (int x, int y, int res)
+//	if (x=0) renvoyer res
+//	if (x mod 2) renvoyer multieg(x-1,y,res+y)
+//	else renvoyer multieg (x/2,y*2,res)
+
+int main()
+
+{
+	return 0;
+}
+			
