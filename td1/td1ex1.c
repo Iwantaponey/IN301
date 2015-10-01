b11f2cbf0d0653bae75e8f0463bf2ddd9c3e92dd
Julie <juju0397@msn.com>
2015-10-01 12:29:06 +0200

    J'ai commenté l'ex1 du td1

--- td1/td1ex1.c
+++ td1/td1ex1.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-int main ()
+#include <stdio.h> // on a besoin d'utiliser la librairie standard pour appeler la fonction printf et afficher du texte
+int main () // fonction du programme
 {
-	int i,j;
-	for (i=0; i<10; i++)
+	int i,j; 
+	for (i=0; i<10; i++) // boucle qui fait 10 itérations
 	{
-		for (j=10; j>i ; j--)
-		{
-			printf (" ");
+		for (j=10; j>i ; j--) // boucle qui fait 9 itérations
+		{ 
+			printf (" "); // affichage d'un espace 
 		}
-		printf ("*\n");
+		printf ("*\n"); // affichage d'une étoile puis retour à la ligne
 	}
 	
-	return 0;
+	return 0; // fin du programme 
 }
