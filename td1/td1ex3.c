06397669929b27b8ad809a9801dc5400982f6c38
Julie <juju0397@msn.com>
2015-10-01 11:22:55 +0200

    J'ai corrigé l'exercice 3 du td1.

--- td1/td1ex3.c
+++ td1/td1ex3.c
@@ -2,24 +2,24 @@
 
 void affiche_resultat(int x, int y)
 {
+	printf("%i x %i \n",x,y);
 	int resultat=0,somme=0;
 	while (x!=1)
 		{
 			if (x%2==0)
 			{
-				x=x/2; y=2*y;
-				if (somme==0) printf("%i x %i \n = ",x,y);
-				else printf ("%i x %i \n = %i x %i + %i ",x,y,x,y,somme);
+				x=x/2; y=2*y;	
 			}
 			else
 			{
-				if (somme==0) printf("%i x %i \n = ",x,y);
 				x=x-1; somme=y+somme;
-				printf ("%i x %i \n = %i x %i + %i ",x,y,x,y,somme);
 			}
+		
+	printf ("= %i x %i + %i \n",x,y,somme);
+		
 		}
 	resultat=y+somme;
-	printf ("\n = %i",resultat);
+	printf ("= %i",resultat);
 }
 
 int main ()
